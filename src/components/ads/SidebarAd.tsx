53a00dcd9ae9cbcf8fcda1feef19046d3818e75e
UFPNGadmin <unlockimfonepng@gmail.com>
2025-06-19 23:29:39 +1000

    Add files via upload

---
+++ src/components/ads/SidebarAd.tsx
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '12f8afa438cd9c820be7ba3d998a3d2f',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//www.highperformanceformat.com/12f8afa438cd9c820be7ba3d998a3d2f/invoke.js"></script>
